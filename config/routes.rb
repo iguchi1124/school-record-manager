da8359aaf2a42e83ae309c3a7beb0005acc05007
Shota Iguchi <e.iguchi1124@gmail.com>
2015-06-18 14:51:07 +0900

    管理機能にダッシュボードを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
 
   devise_for :users
+
+  get 'admin', to: 'admin/dashboards#index', as: 'dashboard'
 
   namespace :admin do
     resources :schools do
       resources :klasses
     end
+
     get 'users', to: 'users#index'
     post 'users/:id', to: 'users#update_role', as: 'update_user_role'
 
