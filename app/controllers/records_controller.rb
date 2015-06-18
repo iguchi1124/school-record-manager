53e8f712fb5c7626de7d82674e49dcb4517f6d36
Shota Iguchi <e.iguchi1124@gmail.com>
2015-06-18 22:56:59 +0900

    :scream_cat: しょうもないミスを修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,13 +1,13 @@
 class RecordsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_category!
+  before_action :require_klass!
 
   def index
   end
 
   private
 
-  def require_category!
+  def require_klass!
     if current_user.klass.nil?
       redirect_to home_path
       flash[:alert] = "所属するクラスを登録してください。"
